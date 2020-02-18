3da39dc6b83a4ee68798fc975c69a05eed2ac629
Nicolas Zein <nicolas.zein@creditas.com.br>
2020-02-18 09:54:36 -0300

    Add command supplier domain

---
+++ catalog/command/configs/database/mapping.py
@@ -0,0 +1,37 @@
+import datetime
+import uuid
+
+from sqlalchemy import Table, Column, String, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import mapper
+
+from catalog.command.configs.database.database import Database
+
+from catalog.command.domain.entities.supplier import Supplier
+
+DATABASE = Database()
+metadata = DATABASE.metadata
+
+
+suppliers = Table(
+    'suppliers',
+    metadata,
+    Column('id', UUID(as_uuid=True), primary_key=True, default=uuid.uuid4()),
+    Column('name', String(255), nullable=False),
+    Column('phone', String(20), nullable=False),
+    Column(
+        'created_at',
+        DateTime(),
+        nullable=False,
+        default=datetime.datetime.utcnow
+    ),
+    Column(
+        'updated_at',
+        DateTime(),
+        nullable=False,
+        default=datetime.datetime.utcnow,
+        onupdate=datetime.datetime.utcnow
+    ),
+)
+
+mapper(Supplier, suppliers)
