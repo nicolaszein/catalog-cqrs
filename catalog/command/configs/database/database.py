67bd69ddd01fed77a2c0344efe0abbbcad94ec8c
Nicolas Zein <nicolas.zein@creditas.com.br>
2020-02-18 08:48:13 -0300

    Add command stack

---
+++ catalog/command/configs/database/database.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+
+from catalog.settings import COMMAND_DATABASE_URL
+
+
+metadata = MetaData()
+
+engine = create_engine(COMMAND_DATABASE_URL)
+Session = sessionmaker(bind=engine)
+
+
+class Database:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+
+        return cls._instance
+
+    def __init__(self):
+        self.__metadata = MetaData()
+        self.__engine = create_engine(COMMAND_DATABASE_URL)
+        self.__session = sessionmaker(bind=engine)()
+
+    @property
+    def metadata(self):
+        return self.__metadata
+
+    @property
+    def session(self):
+        return self.__session
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='catalog-cqrs',
+    packages=[''],
+    version='0.0.1',
+    description="",
+    long_description="",
+    license='',
+    include_package_data=True,
+    tests_require=[],
+)
